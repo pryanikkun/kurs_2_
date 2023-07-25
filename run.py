32f0c45511eabd4dea5e1190b3796d11fcf6fae2
Name LastName <name56464@domen.ru>
2023-07-25 19:45:37 +0300

    добавился bookmarks_blueprint

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from app.posts.views import posts_blueprint
 from app.api.views import api_blueprint
+from app.bookmarks.views import bookmarks_blueprint
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 app.register_blueprint(posts_blueprint)
 app.register_blueprint(api_blueprint)
+app.register_blueprint(bookmarks_blueprint)
 
 if __name__ == '__main__':
     app.run()
